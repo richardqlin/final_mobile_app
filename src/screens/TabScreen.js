11e262070ca84b22186b08fd699bc9a0a0b5e868
richardqlin <richardqlin@gmail.com>
2020-12-12 17:14:23 -0800

    Update TabScreen.js

--- src/screens/TabScreen.js
+++ src/screens/TabScreen.js
@@ -16,7 +16,7 @@ this.submit = this.submit.bind(this);
 submit(title,post) {
 
 if (title && post) {
-    fetch('https://murmuring-bastion-31969.herokuapp.com/post',{
+    fetch('https://protected-spire-82809.herokuapp.com/post',{
     method: 'POST',
     headers: {
         'Content-Type':'application/x-www-form-urlencoded'},
